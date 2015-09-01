6661cee81d9c237c876e4060b11ffc3be4f744d8
Silver90 <ignazio.lipari90@libero.it>
2015-09-01 13:17:20 +0200

    remove phundament configuration and add nginx,php-fpm,mariadb configuration and container

--- app/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sh /app/src/init.sh
-
-echo "Container keep running to improve stack-stability, if you are running an interactive shell or one-off command use <ctrl+c> to exit."
-tail -f /dev/null
